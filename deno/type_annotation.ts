38060a0f94985555a58b36483fddc253de2ccfae
Yamamori <moriyama14t@gmail.com>
2023-03-07 21:38:21 +0900

    type-annotation

---
+++ deno/type_annotation.ts
@@ -0,0 +1,4 @@
+// https://typescriptbook.jp/reference/values-types-variables/type-annotation
+
+// TypeScriptでは変数宣言するときに、その変数にどんな値が代入可能かを指定できます。その指定のことを型注釈(type annotation; 型アノテーション)と言う
+const count: number = 123;
